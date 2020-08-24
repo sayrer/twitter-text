e544eff95953348385b43443b5e92cdeec7e24ee
Robert Sayre <sayrer@gmail.com>
2020-08-23 20:49:46 -0700

    Ruby seems to work

--- rust_bindings/ruby/demo.rb
+++ rust_bindings/ruby/demo.rb
@@ -2,4 +2,4 @@ require "rust_bindings/ruby/twittertext"
 
 autolinker = Twittertext::Autolinker.new
 
-puts autolinker.autolink "What's up $TWTR or #TWTR or @twitter?"
+puts autolinker.autolink "Just setting up my $TWTR or #TWTR or @twitter? Please visit https://twiter.com."
