00bce32be43896bd0fea19536eb3842458edd0a6
sayrer <sayrer@gmail.com>
2025-11-25 13:03:34 -0800

    Update vendor

--- rust_bindings/cargo/vendor/proc-macro-error-0.4.8/tests/macro-errors.rs
+++
@@ -1,9 +0,0 @@
-extern crate rustversion;
-extern crate trybuild;
-
-#[rustversion::attr(any(before(1.39), not(stable)), ignore)]
-#[test]
-fn ui() {
-    let t = trybuild::TestCases::new();
-    t.compile_fail("tests/ui/*.rs");
-}
