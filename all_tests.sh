4094e65f2fee47d36f96a2f21955054b80fcb075
sayrer <sayrer@gmail.com>
2025-12-06 09:13:10 -0800

    Update ruby layout.

--- all_tests.sh
+++ all_tests.sh
@@ -4,7 +4,5 @@ bazel test //rust/... && \
 bazel test //rust_bindings/cpp/... && \
 bazel test //rust_bindings/cpp_sanitizers/... && \
 bazel test //rust/python-bindings/test/... && \
-#bazel build //rust/ruby-bindings:twittertext && \
-#ln -sf ../../bazel-bin/rust/ruby-bindings/twittertext.bundle rust_bindings/ruby/twittertext.bundle && \
-#/opt/homebrew/opt/ruby@3.2/bin/ruby -I . -e "require 'rspec/autorun'; Dir['rust_bindings/ruby/spec/*_spec.rb'].sort.each { |f| load f }"
+#bazel test //rust/ruby-bindings/spec/... && \
 echo "Done."
