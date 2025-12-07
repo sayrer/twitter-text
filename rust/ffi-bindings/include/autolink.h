a9f6b1c801db7af7fbf373d02b9a5c93ebadcd72
sayrer <sayrer@gmail.com>
2025-12-07 15:30:00 -0800

    Added Swift, not running yet, all tests pass.

--- rust/ffi-bindings/include/autolink.h
+++ rust/ffi-bindings/include/autolink.h
@@ -3,6 +3,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include "twitter_text_modifiers.h"
 
 typedef struct TwitterTextAutolinker TwitterTextAutolinker;
 
@@ -122,52 +123,6 @@ char* twitter_text_autolinker_autolink_cashtags(
 void twitter_text_string_free(char* str);
 
 /* Link Attribute Modifiers */
-typedef struct TwitterTextAddAttributeModifier TwitterTextAddAttributeModifier;
-typedef struct TwitterTextReplaceClassModifier TwitterTextReplaceClassModifier;
-
-/* Entity types for modifier filtering */
-typedef enum {
-    TWITTER_TEXT_ENTITY_URL = 0,
-    TWITTER_TEXT_ENTITY_HASHTAG = 1,
-    TWITTER_TEXT_ENTITY_MENTION = 2,
-    TWITTER_TEXT_ENTITY_CASHTAG = 3,
-} TwitterTextEntityType;
-
-/* Create an AddAttributeModifier that adds a custom attribute to specific entity types */
-TwitterTextAddAttributeModifier* twitter_text_add_attribute_modifier_new(
-    const TwitterTextEntityType* entity_types,
-    size_t entity_types_count,
-    const char* key,
-    const char* value
-);
-
-void twitter_text_add_attribute_modifier_free(TwitterTextAddAttributeModifier* modifier);
-
-/* Create a ReplaceClassModifier that replaces the class attribute */
-TwitterTextReplaceClassModifier* twitter_text_replace_class_modifier_new(const char* new_class);
-
-void twitter_text_replace_class_modifier_free(TwitterTextReplaceClassModifier* modifier);
-
-/* Set a modifier on an autolinker */
-void twitter_text_autolinker_set_add_attribute_modifier(
-    TwitterTextAutolinker* autolinker,
-    TwitterTextAddAttributeModifier* modifier
-);
-
-void twitter_text_autolinker_set_replace_class_modifier(
-    TwitterTextAutolinker* autolinker,
-    TwitterTextReplaceClassModifier* modifier
-);
-
-/* Link Text Modifier - Callback-based API */
-
-/* Entity structure for callbacks */
-typedef struct {
-    TwitterTextEntityType entity_type;
-    int32_t start;
-    int32_t end;
-} TwitterTextCEntity;
-
 /* Callback function type for modifying link text
  * Returns a new C string that must be freed by the caller using twitter_text_string_free
  */
