aaf449933dd090f78a8c966aee17f2224904da0d
Robert Sayre <sayrer@gmail.com>
2025-12-20 08:48:27 -0800

    Remove test script.

--- all_tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Run rust tests (may fail if Ruby < 3.3, but that's OK)
-bazel test //rust/... || true
-
-# Run cpp tests
-bazel test //rust/cpp-bindings/... && \
-bazel test //rust/cpp-sanitizer-bindings/... && \
-echo "Done."
